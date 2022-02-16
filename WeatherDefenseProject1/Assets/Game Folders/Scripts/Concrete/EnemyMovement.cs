f26a183ed4c2ae3c4d280a9b59c86e75ed54a087
Can Karaca <cankaraca94@gmail.com>
2022-02-16 22:25:56 +0300

    Added waypoint system for enemy movement, will organize and develop

---
+++ WeatherDefenseProject1/Assets/Game Folders/Scripts/Concrete/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float _moveSpeed = 10f;
+
+    private Transform _target;
+    private int _wayPointIndex = 0;
+
+    private void Start()
+    {
+        _target = WayPoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = _target.position - transform.position;
+
+        transform.Translate(direction.normalized * _moveSpeed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, _target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        _wayPointIndex++;
+        _target = WayPoints.points[_wayPointIndex];
+    }
+}
